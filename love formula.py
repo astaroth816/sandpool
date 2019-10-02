3f9810ec5db4a10c3c7d0a7aee4c820eb66fcbb5
Chia-Hao Tang <p7476762@gmail.com>
2019-10-02 21:35:56 +0800

    Add files via upload

---
+++ love formula.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 19:50:17 2019
+
+@author: andy3
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+t = np.linspace(0,math.pi, 1000)
+x = np.sin(t)*np.sin(t)*np.sin(t)*16
+y = np.cos(t)*13-np.cos(2*t)*5-np.cos(3*t)*2-np.cos(3.5*t)
+plt.plot(x, y, color='red', linewidth=2)
+plt.plot(-x, y, color='red', linewidth=2)
+plt.ylim(-20, 15)
+plt.xlim(-25, 25)
+plt.show()
