d5954f011b0f734555cf61547ea6d887988e8a0b
Chia-Hao Tang <p7476762@gmail.com>
2019-10-05 12:44:57 +0800

    Dear Ally

--- love_formula.py
+++ love_formula.py
@@ -9,12 +9,23 @@ Created on Thu Sep 26 19:50:17 2019
 import matplotlib.pyplot as plt
 import numpy as np
 import math
- 
-t = np.linspace(0, math.pi, 1000)
+t = np.linspace(0,math.pi, 1000)
 x = np.sin(t)*np.sin(t)*np.sin(t)*16
 y = np.cos(t)*13-np.cos(2*t)*5-np.cos(3*t)*2-np.cos(3.5*t)
 plt.plot(x, y, color='red', linewidth=2)
 plt.plot(-x, y, color='red', linewidth=2)
-plt.ylim(-20, 15)
-plt.xlim(-25, 25)
+s = np.linspace(0,math.pi, 1000)
+z = 1.5*(np.sin(t)*np.sin(t)*np.sin(t)*16)
+w = 2+1.5*(np.cos(t)*13-np.cos(2*t)*5-np.cos(3*t)*2-np.cos(3.5*t))
+plt.plot(z, w, color='red', linewidth=2)
+plt.plot(-z, w, color='red', linewidth=2)
+plt.ylim(-40, 50)
+plt.xlim(-50, 50)
+s = np.linspace(0,math.pi, 1000)
+z = 2*(np.sin(t)*np.sin(t)*np.sin(t)*16)
+w = 4+2*(np.cos(t)*13-np.cos(2*t)*5-np.cos(3*t)*2-np.cos(3.5*t))
+plt.plot(z, w, color='red', linewidth=2)
+plt.plot(-z, w, color='red', linewidth=2)
+plt.ylim(-40, 50)
+plt.xlim(-50, 50)
 plt.show()
