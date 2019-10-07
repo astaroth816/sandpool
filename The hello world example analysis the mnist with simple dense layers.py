74af439ae284ed409209577351e6a9ef37f753c1
Ally Lin <llin@ilink.com.tw>
2019-10-07 18:42:48 +0800

    dnn add evaluate

--- The hello world example analysis the mnist with simple dense layers.py
+++ The hello world example analysis the mnist with simple dense layers.py
@@ -62,55 +62,45 @@ Neural_network.add(layers.Dense(10,
 Neural_network.compile(optimizer='rmsprop',
                        loss='categorical_crossentropy',
                        metrics=['accuracy'])
+
 print("Our DNN can be described by: ")
 Neural_network.summary()
 #We also need to label the input_images with categorical compiler.
 from keras.utils import to_categorical
 
 train_labels=to_categorical(train_labels)
-
 test_labels=to_categorical(test_labels)
 
-
-
-#Start to train our NN with 10 times.
-
+#Start to train our NN with 20 times.
 history=Neural_network.fit(train_images,
                            train_labels,
                            epochs=20,
                            batch_size=128)
 
+# Evaluate
+_, accuracy = Neural_network.evaluate(test_images, test_labels)
+print('Loss: %.6f' % _, ', Accuracy: %.2f' % (accuracy*100))
+
 #Show the accuracy rate with each training.
 import matplotlib.pyplot as plt
 
 acc=history.history['acc']
-
 loss=history.history['loss']
-
 epochs=range(1,len(acc)+1)
 
 plt.plot(epochs,
          acc,
          'bo',
          label='Traning accuracy')
-
 plt.title('The accuracy of training ')
-
 plt.legend()
 
 #Show the loss rate with each training.
-
-plt.figure()
-
 plt.plot(epochs,
          loss,
          'g',
          label='Training loss quantity')
-
 plt.title('Training loss')
-
 plt.legend()
 
-
 plt.show()
-
